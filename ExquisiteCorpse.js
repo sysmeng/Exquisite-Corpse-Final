4c94857dd2afd7d67972d670b498b7a5233369e7
sysmeng <zm2573@nyu.edu>
2026-04-19 16:18:58 -0400

    file reorg, tile draft working

---
+++ ExquisiteCorpse.js
@@ -0,0 +1,116 @@
+let camPhoto;
+let fontData;
+let gameState;
+let camX, camY;
+let scale;
+let altPhoto;
+
+let tileArray=[];
+let tileW,tileH;
+
+//let testImg;
+
+function preload(){
+  //
+  camPhoto=loadImage('data/JermaTestImg.png');
+  //testImg=loadImage('data/NLTestImg.jpg');
+}
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  background(0);
+  gameState = 3;
+
+  tileW=4;
+  tileH=4;
+
+  //console.log(scale);
+  imageMode(CENTER);
+  camPhoto.resize(0,height-200);
+
+  camPhoto.loadPixels();
+ // put setup code here
+ // init random array for tiles
+ 
+}
+
+function draw() {
+  // put drawing code here
+  //image(camPhoto,width/2,height/2);
+
+  scramblephoto();
+  if (gameState=3){
+    // photo state
+    // takephoto();
+  } else if (gameState=4){
+    // generate the distortions
+    //tileGenerate();
+  } else{
+    //gameloop();
+  }
+  
+}
+
+
+
+function takephoto(){
+  //
+  //generate button
+  //overlay for facial alignment
+  //load webcam to cam photo
+}
+
+function scramblephoto(){
+  //
+  //camPhoto.loadPixels(); 
+  //console.log(camPhoto);
+  var temptileW = int(camPhoto.width/tileW);
+  var temptileH = int(camPhoto.height/tileH);
+
+  console.log(temptileW,temptileH);
+  for(k=0;k<tileH;k++){
+    for(i=0;i<tileW;i++){
+      var tempTileArr = camPhoto.get(temptileW*i,temptileH*k,temptileW,temptileH);
+      tileArray.push(tempTileArr);
+  }
+  }
+  //console.log(tileArray);
+  
+  for (i=0;i<tileArray.length;i++){
+    //
+    var tempTileImg= tileArray[i];
+    //image(tempTileImg,int(random(0,width)),int(random(0,height)));//debug to test print tiles
+    var operationRand
+    //filter(INVERT);
+  }
+  //console.log(tileArray[0]);
+  //image(tileArray[0],width/2,height/2)
+
+  for(i=0;i<(tileW*tileH);i++){
+    //var tempTileArr = pixels (camPhoto);
+    //console.log(tempTileArr);
+    
+    //
+  }
+  noLoop();
+}
+
+function tileGenerate(){
+  //
+  altPhoto.loadPixels(); // load image pixels
+  
+    // take cut of image
+    // fill selection with transparent pixels
+    // record position
+    // add tile to array
+  //scramble tiles
+  //catch important features with facial detection
+  //replace cut areas with transparent pixels
+  //use get() to divy out tiles
+}
+
+function gameloop(){
+  //
+  //update 
+  //redraw
+}
---
+++ tiles.js
@@ -0,0 +1,22 @@
+//
+
+class tile{
+
+    constructor (img,posVect){ //pass image section
+        this.tileImg=img;
+        this.pos=posVect;
+
+    }
+
+    update(){
+        //
+    }
+
+    graphics(){
+        //
+    }
+
+    scramble(){
+        //
+    }
+}
